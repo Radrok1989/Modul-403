0a32f33b7b2d6a392666de6b6d53e6c7c4d17486
Radrok1989 <carloumberto1989@gmx.ch>
2021-11-02 20:55:17 +0100

    commit

--- While-Schleife/Program.cs
+++ While-Schleife/Program.cs
@@ -10,6 +10,53 @@ namespace While_Schleife
     {
         static void Main(string[] args)
         {
+            /*try
+            {
+                double d = 0;
+                double ins = 0;
+            Console.WriteLine("Geben sie eine Zahl zwischen 1 und 20 ein");
+                ins = Convert.ToDouble(Console.ReadLine());
+                double count = 1;
+                if (ins >= 1 && ins <= 20)
+                {
+                    while (count <= ins)
+                    {
+                        d += count;
+                        count++;
+                    }
+                    Console.WriteLine($"Der Durchschnitt beträgt {d / ins}");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();*/
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("If you write stop, i will stop!");
+                    string insert = Console.ReadLine();
+                    if (insert == "stop")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("ok");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You didn't say stop");
+                        Console.Clear();
+                    }
+                }
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
