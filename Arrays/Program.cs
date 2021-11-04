2cae09bd72cc428d99e416b666087597ac626884
Radrok1989 <carloumberto1989@gmx.ch>
2021-11-04 21:33:47 +0100

    array

---
+++ Arrays/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] array = new int[5];
+            array[0] = 10;
+            array[1] = -3;
+            array[2] = 7;
+            array[3] = 24;
+            array[4] = 50;
+
+            double[] arrayd = {3.4, 5.1, 4.2, 10.7, 8.9};
+
+            string[] arrays = { "Basil","Marco","Oliver","Lucian","Nitharsan" };
+
+            for (int i = 0; i<array.Length; i++)
+            {
+                Console.Write("\t"+arrays[i]);
+            }
+
+            Console.WriteLine(); ;
+
+            foreach(var x in array)
+            {
+                Console.Write("\t" + x);
+            }
+            Console.WriteLine();
+            int max = array[0], min = array[0];
+            double d = 0;
+            for (int i= 0; i<array.Length;i++)
+            {
+                if (array[i]>max)
+                {
+                    max = array[i];
+                }
+                else if (array[i]<min)
+                {
+                    min = array[i];
+                }
+                d += array[i];
+
+
+            }
+            Console.WriteLine($"\tMax = {max}, Min = {min}, Durchschnitt = {d/array.Length}");
+            double miau = 0;
+            foreach(var x in array)
+            {
+                if (x > max)
+                {
+                    max = x;
+                }
+                else if (x < min)
+                {
+                    min = x;
+                }
+                miau += x;
+            }
+            Console.WriteLine($"\tMax = {max}, Min = {min}, Durchschnitt = {d / array.Length}");
+            */
+            int[] array = { 3, 7, 1, 8, 2, 9, 4, 10, 5, 6 };
+            Array.Sort(array);
+            foreach(var x in array)
+            {
+                Console.WriteLine("\t"+x);
+            }
+            Console.WriteLine($"\n\n Max = {array[array.Length-1]}, Min = {array[0]}");
+
+            int k = 0;
+            int[] even = new int[array.Length];
+            foreach(var x in array)
+            {
+                if(x % 2 == 0)
+                {
+                    even[k++] = x;
+                    
+                }
+            }
+            foreach(var x in even)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
--- While-Schleife/Program.cs
+++ While-Schleife/Program.cs
@@ -33,7 +33,7 @@ namespace While_Schleife
             }
             Console.ReadKey();*/
 
-            try
+            /*try
             {
                 while (true)
                 {
@@ -56,7 +56,190 @@ namespace While_Schleife
             catch(Exception e)
             {
                 Console.WriteLine(e.Message);
+            }*/
+
+            /*while (true)
+            {
+            try
+            {
+                Console.Write("Mit j/J weiter, mit n/N ende.");
+                string str = Console.ReadLine();
+                Console.Clear();
+                if (str == "n" || str == "N")
+                {
+                    Console.WriteLine("Ende");
+                    break;
+                }
+                if (str == "j" || str == "J")
+                {
+                    Console.WriteLine("\n Wählen sie bitte:");
+                    Console.WriteLine("\n Addieren \t\t(1)");
+                    Console.WriteLine("\n Subtrahieren \t\t(2)");
+                    Console.WriteLine("\n Multiplizieren \t(3)");
+                    Console.Write("\n Dividieren \t\t(4) \t");
+                    int x = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("\n Geben sie die erste Zahl ein: ");
+                    double first = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("\n Geben sie die zweite Zahl ein: ");
+                    double second = Convert.ToInt32(Console.ReadLine());
+
+                    switch(x)
+                    {
+                        case 1:
+                            Console.WriteLine($"{first} + {second} = {first + second}");
+                            break;
+                        case 2:
+                            Console.WriteLine($"{first} - {second} = {first - second}");
+                            break;
+                        case 3:
+                            Console.WriteLine($"{first} * {second} = {first * second}");
+                            break;
+                        case 4:
+                            if (second == 0)
+                            {
+                                Console.WriteLine("Imagine that you have zero cookies and you split them evenly among zero friends. How many cookies does each person get?");
+                                Console.WriteLine("See ? It doesn’t make sense.And Cookie Monster is sad that there are no cookies.And you are sad that you have no friends.");
+                                break;
+                            }
+                            Console.WriteLine($"{first} : {second} = {first / second}");
+                            break;
+                        default:
+                            Console.WriteLine("Musse du scho richtige Sache eingeben!");
+                            break;
+
+
+                    }
+
+                }
+
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }*/
+
+           /* while (true)
+            {
+                try
+                {
+                    Console.Write("Mit j/J weiter, mit n/N ende.");
+                    string str = Console.ReadLine();
+                    Console.Clear();
+                    if (str == "n" || str == "N")
+                    {
+                        Console.WriteLine("Ende");
+                        break;
+                    }
+                    if (str == "j" || str == "J")
+
+                    {
+                        Console.WriteLine("\n Wählen sie bitte:");
+                        Console.WriteLine("\n Alter berechnen \t\t(1)");
+                        Console.WriteLine("\n Rabatt 3% berechnen \t\t(2)");
+                        Console.Write("\n Urlaubstage berechnen \t\t(3): ");
+
+                        int y = Convert.ToInt32(Console.ReadLine());
+
+                        switch (y)
+                        {
+                            case 1:
+                                Console.Write($"Geben Sie Ihren Jahrgang ein ");
+                                int year = Convert.ToInt32(Console.ReadLine());
+                                if (year >= 1901 && year <= 2021)
+                                {
+                                    Console.WriteLine($"Sie sind {2021 - year} Jahre alt");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Das kann nicht sein!");
+                                    break;
+                                }
+                            case 2:
+                                Console.Write($"Geben Sie den Betrag ein. ");
+                                double money = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine($"3% von Ihrem Betrag sind: {money * 0.03}");
+
+                                break;
+                            case 3:
+                                Console.Write($"Wie viele Monate haben sie dieses Jahr gearbeitet? ");
+                                double arbeit = Convert.ToDouble(Console.ReadLine());
+                                Console.WriteLine($"Sie haben {arbeit * 2,4} Tage Urlaub zu gute.");
+                                break;
+
+                            default:
+                                Console.WriteLine("Musse du scho richtige Sache eingeben!");
+                                break;
+                        }
+
+                    }
+                    Console.ReadKey();
+                }
+               
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }*/
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Gib ein a, b, oder c");
+                    string i = Console.ReadLine();
+                    Console.Clear();
+                    switch(i)
+                    {
+                        case "a":
+                            Console.WriteLine("Du bist der beste.");
+                            break;
+                        case "A":
+                            Console.WriteLine("Du bist der beste.");
+                            break;
+                        case "b":
+                            Console.WriteLine("Du bist nicht der beste, aber immerhin zweiter.");
+                            break;
+                        case "B":
+                            Console.WriteLine("Du bist nicht der beste, aber immerhin zweiter.");
+                            break;
+                        case "c":
+                            Console.WriteLine("Du solltest noch etwas weiterüben.");
+                            break;
+                        case "C":
+                            Console.WriteLine("Du solltest noch etwas weiterüben.");
+                            break;
+
+                        default:
+                            Console.WriteLine("Du solltest a, b, und c schon von anderen Zeichen unterscheiden können.");
+                            break;
+                    }
+                    Console.WriteLine("______________________________________");
+                    Console.WriteLine();
+                    Console.WriteLine("Falls du das Programm beenden möchtest drücke n/N");
+                    string exit = Console.ReadLine();
+                    if (exit == "n" || exit == "N")
+                    {
+                        break;
+                    }
+                    
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+
+            }
+           
         }
     }
 }
+
+
+// a du bist super, b du bist 2ter, c übe weiter  if =n break
