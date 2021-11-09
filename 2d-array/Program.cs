8415f95ac4c8fd132f2454bc6c5965dd62c7ed50
Radrok1989 <carloumberto1989@gmx.ch>
2021-11-09 21:39:39 +0100

    aaa

---
+++ 2d-array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2d_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[3, 4] { { 3, 2, 7, 5 }, { 8, 5, 10, 3 }, { 1, 5, 2, 9 } };
+            for(int i = 0; i<array.GetLength(0);i++)
+            {
+                for (int j = 0; j < array.GetLength(1);j++)
+                {
+                    Console.Write("\t"+array[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
--- Strings/Program.cs
+++ Strings/Program.cs
@@ -10,6 +10,52 @@ namespace Strings
     {
         static void Main(string[] args)
         {
+            //Aufgabe 1
+            /*string str = "Hallo Basil @ Schmid";
+
+            Console.WriteLine("1ster Buchstabe: "+str[10]);
+
+            //str = str.Replace("Schmid", "Wow");
+            Console.WriteLine(str);
+            //str=str.ToUpper();
+            Console.WriteLine(str);
+            string str2 = "Nick", str3 = "Basil", str4;
+            Console.WriteLine($"{str2}, {str3} sind gleich? : {str2.CompareTo(str3)}");
+            Console.WriteLine(str.Contains("Hallo Basil"));
+
+            string s = str2 + " " + str3;
+            Console.WriteLine(s);
+
+            Console.WriteLine("Substring str von 3-7" + str.Substring(3,7));
+
+            str = "Hallo A21 wie geht es euch? Alles klar?";
+            int cntl = 0, cnte = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]==' ')
+                {
+                    cntl++;
+                }
+                else if (str[i]=='e' || str[i]=='E')
+                {
+                    cnte++;
+                }
+            }
+            Console.WriteLine($"der String enthielt {cntl} Leerzeichen und {cnte} e's und E's");
+            */
+            //Aufgabe 2
+            /*
+            string s = "sdrawkcab gnitnirP";
+            for(int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[s.Length - i - 1]);
+            }
+            */
+            //Aufgabe 3
+           
+
+
+            Console.ReadKey();
         }
     }
 }
