1f50572f63e2791a060eeedceaa4426be767413c
Radrok1989 <carloumberto1989@gmx.ch>
2022-01-22 16:41:41 +0100

    aaa

---
+++ Collections/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> liste = new List<double>() { 7.3, 3.1, 6.5 };
+
+            foreach (var x in liste)
+            {
+                Console.Write($"\t {x}");
+            }
+            Console.WriteLine();
+            liste.Add(99.99);
+
+            foreach (var x in liste)
+            {
+                Console.Write($"\t {x}");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine($"\tAnzahl der Elemente: {liste.Count}");
+            Console.WriteLine("\n");
+            liste.Sort();
+            foreach (var x in liste)
+            {
+                Console.Write($"\t {x}");
+            }
+            liste.Add(6.5);
+            liste.Remove(6.5);
+            Console.WriteLine("\n");
+
+            foreach (var x in liste)
+            {
+                Console.Write($"\t {x}");
+            }
+
+            List<string> listeS = new List<string>() { "Basil", "Oliver", "Gian-Marco", "Alain", "Carlo" };
+            //listeS.Clear();
+            listeS.Add("Sarah");
+            //listeS.Remove("Alain");
+            if (listeS.Contains("Carlo"))
+                listeS.Remove("Carlo");
+            Console.WriteLine();
+            foreach (var x in listeS)
+            {
+                Console.Write($"\t {x}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sortierte listen und dictionaries/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sortierte_listen_und_dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<string, string> liste = new SortedList<string, string>();
+            liste.Add("one", "Z");
+            liste.Add("two", "H");
+            liste.Add("three", "A");
+            liste.Add("four", "I");
+            liste.Add("eight", "B");
+
+            for(int i=0;i<liste.Count;i++)
+            {
+                Console.WriteLine($"\t Key = {liste.Keys[i]}, Value = {liste.Values[i]}");
+            }
+            Console.WriteLine("\n\n");
+            foreach(KeyValuePair<string, string> k in liste)
+            {
+                Console.WriteLine($"\t Key = {k.Key}, Value = {k.Value}");
+            }
+
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            dict.Add("one", "Z");
+            dict.Add("two", "H");
+            dict.Add("three", "A");
+            dict.Add("four", "I");
+            dict.Add("eight", "B");
+
+            foreach (KeyValuePair<string, string> d in dict)
+            {
+                Console.WriteLine($"\t Key = {d.Key}, Value = {d.Value}");
+            }
+
+
+            /*  foreach (var x in liste)
+              {
+                  Console.Write($"\t{x}");
+              }*/
+
+            Console.ReadKey();
+        }
+    }
+}
