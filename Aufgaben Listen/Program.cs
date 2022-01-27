3d3653391986d1d5023d37538593f330271d70d2
Radrok1989 <carloumberto1989@gmx.ch>
2022-01-27 19:36:58 +0100

    aaa

---
+++ Aufgaben Listen/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Aufgaben_Listen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 Erstellen Sie eine Liste mit Namen der Klasse ILB A21
+            List<string> ilba21 = new List<string>() { "Basil", "Alain", "Beni", "Sarah", "Nithi", "Lucian", "Gian-Marco", "Oli", "Nick", "Carlo" };
+
+            //2 Lassen Sie die Liste auf dem Monitor anzeigen
+            foreach (var x in ilba21)
+            {
+                Console.Write($"\t {x}");
+            }
+            Console.WriteLine();
+
+            //3 Löschen Sie das 5 Element
+            ilba21.RemoveAt(4);
+
+            //4 Fügen Sie das Element wieder hinzu
+            ilba21.Add("Nithi");
+
+            //5 Untersuchen Sie in der Liste ob Batman existiert. Wenn nicht fügen sie ihn hinzu.
+            if (ilba21.Contains("Batman"))
+            {
+                Console.WriteLine("Batman ist schon enthalten in der Liste");
+            }
+            else
+            {
+                ilba21.Add("Batman");
+            }
+
+            //6 Sortieren Sie die Liste und geben sie die Namen auf dem Monitor aus
+            ilba21.Sort();
+            foreach (var x in ilba21)
+            {
+                Console.Write($"\t {x}");
+            }
+            Console.WriteLine();
+
+            //7 Listen Sie alle Namen auf dem Bildschirm und wenn Carlo als Carlos geschrieben ist, bitte korrigieren Sie es
+            if (ilba21.Contains("Carlos"))
+            {
+                ilba21.Remove("Carlos");
+                ilba21.Add("Carlo");
+            }
+            foreach (var x in ilba21)
+            {
+                Console.Write($"\t {x}");
+            }
+            Console.WriteLine();
+
+            //8 Listen Sie alles rückwerts auf.
+            ilba21.Reverse();
+            foreach (var x in ilba21)
+            {
+                Console.Write($"\t {x}");
+            }
+            Console.WriteLine();
+
+            //9 Zählen Sie alle Namen die mit S anfangen
+            int count = 0;
+            for(int i=0;i<ilba21.Count;i++)
+            {
+                bool result;
+                result = ilba21[i].StartsWith("S");
+                if (result == true)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"In dieser Klasse gibt es {count} Person/en*in/innen die mit S beginnen.");
+
+            //11 Erstellen Sie eine Liste mit {10,1,9,2,8,3,7,4,6,5}, dann fügen sie die Zahl -3,-1, 50 und 100. Finden Sie Max,Min, Durchschnitt, Anzahl der gerade, Anzahl der Gerade und der negativen Zahlen.Zeigen Sie alle Ergebnisse im Bildschirm.
+
+            List<int> zahlen = new List<int>() { 10, 1, 9, 2, 8, 3, 7, 4, 6, 5 };
+            zahlen.Add(-3);
+            zahlen.Add(-1);
+            zahlen.Add(50);
+            zahlen.Add(100);
+            int max = zahlen[0]; int min = zahlen[0];
+            double d = 0;
+            int even = 0;
+            int odd = 0;
+            int sub = 0;
+            for(int i=0;i<zahlen.Count;i++)
+            {
+                if (zahlen[i] > max)
+                    max = zahlen[i];
+                if (zahlen[i] < min)
+                    min = zahlen[i];
+                if (zahlen[i] % 2 == 0)
+                    even++;
+                if (zahlen[i] % 2 == 1)
+                    odd++;
+                if (zahlen[i] < 0)
+                    sub++;
+                d += zahlen[i];
+            }
+            Console.WriteLine($"Max = {max}, Min = {min}, Gerade Zahlen = {even}, Ungerade Zahlen = {odd}, Negative Zahlen = {sub}, Durchschnitt = {d/zahlen.Count}");
+
+            //12 Erstellen sie eine ListeU und ListeG und übertragen sie alle geraden/ungeraden Zahlen
+            List<int> listeg = new List<int>();
+            List<int> listeu = new List<int>();
+            for(int i = 0;i<zahlen.Count;i++)
+            {
+                if (zahlen[i] % 2 == 0)
+                    listeg.Add(zahlen[i]);
+                else
+                    listeu.Add(zahlen[i]);
+            }
+            Console.WriteLine("Die geraden Zahlen sind: ");
+            foreach (var x in listeg)
+                Console.Write($"\t {x}");
+            Console.WriteLine("\nDie ungeraden Zahlen sind: ");
+            foreach (var x in listeu)
+                Console.Write($"\t {x}");
+            Console.WriteLine();
+
+            //13 Quadratsumme aus listeg und listeu
+            int qsug = 0;
+            int qsuu = 0;
+            foreach (var x in listeg)
+                qsug += x * x;
+            foreach (var x in listeu)
+                qsuu += x * x;
+            Console.WriteLine("Quadratsumme aus den Ungeraden: "+qsuu);
+            Console.WriteLine("Quadratsumme aus den Geraden: "+qsug);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ stack und queue/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+
+namespace stack_und_queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack
+            
+            Stack karten = new Stack();
+            karten.Push("Herz 2");
+            karten.Push("Karo 3");
+            karten.Push("Pik 7");
+            karten.Push("Kreuz 10");
+            karten.Push("Herz Ass");
+            karten.Push("Pik König");
+
+            //Das oberste aus dem Stack ausgeben und dann entfernen
+            Console.WriteLine(karten.Pop()+"\n\n");
+
+            //Das oberste aus dem Stack entfernen
+            karten.Pop();
+
+            //Den Stack von oben nach unten ausgeben
+            foreach (var x in karten)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine(karten.Peek());
+            
+            //Praktische Aufgabe
+            Console.WriteLine("\n\n");
+            Stack a21 = new Stack();
+            a21.Push("Basil");
+            a21.Push("Gian-Marco");
+            a21.Push("Oliver");
+            a21.Push("Sarah");
+            a21.Push("Alain");
+            a21.Push("Beni");
+
+            foreach(var x in a21)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\n\n");
+
+            foreach(var x in a21)
+            {
+                if(x=="Oliver")
+                {
+                    break;
+                }
+
+                Console.WriteLine(x);
+
+            }
+            a21.Push("Carlo");
+            a21.Push("Nick");
+
+            Console.WriteLine("\n\n");
+
+            foreach (var x in a21)
+            {
+                Console.WriteLine(x);
+            }
+            
+
+            //Queue
+            Queue warteschlange = new Queue();
+            //Der Queue etwas hinzufügen
+            warteschlange.Enqueue("Basil");
+            warteschlange.Enqueue("Batman");
+            warteschlange.Enqueue("Superman");
+            warteschlange.Enqueue("Joker");
+            warteschlange.Enqueue("Iron Man");
+            warteschlange.Enqueue("Captain America");
+            warteschlange.Enqueue("Green Arrow");
+
+
+            //Das Ende der Warteschlange ausgeben
+            Console.WriteLine(warteschlange.Peek()+"\n\n");
+
+            //Warteschlange ausgeben
+            foreach(var x in warteschlange)
+            {
+                Console.WriteLine(x);
+            }
+            //Den vordersten in der Warteschlange rausnehmen. Mit cw kann man es auch anzeigen
+            warteschlange.Dequeue();
+            
+            // Die 3 vordersten in der Warteschlange rausnehmen
+            for (int i = 0; i < 3; i++)
+                warteschlange.Dequeue();
+
+
+            Console.ReadKey();
+        }
+    }
+}
